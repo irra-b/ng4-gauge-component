86ea8422c4f8e740ba374ea8b14d94bb466a7cea
irra_b <irra_b@yahoo.es>
2017-11-05 19:01:19 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  sections:any=[];
+  areas:any=[];
+  constructor(){
+    this.sections.push({start: 0, end: 25, color: 'rgba(255, 0, 0, 0.5)'});
+    this.sections.push({start: 25, end: 50, color: 'rgba(255, 255, 0, 0.5)'});
+    this.sections.push({start: 50, end: 100, color: 'rgba(0, 255, 0, 0.5)'});
+    this.areas.push({start: 0, end: 24, color: 'rgba(255, 0, 0, 0.3)'});
+  }
 }
